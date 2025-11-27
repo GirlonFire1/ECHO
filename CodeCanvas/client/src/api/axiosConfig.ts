da7c710c5e028f90ea001e0d952676e95de28a24
GirlonFire1 <ankitatalukdar2003@gmail.com>
2025-11-27 11:55:28 +0530

    Fix API URL

--- CodeCanvas/client/src/api/axiosConfig.ts
+++ CodeCanvas/client/src/api/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL
   ? `${import.meta.env.VITE_API_URL}/api/v1`
   : import.meta.env.MODE === 'production'
-    ? 'https://echo-backend-vlsc.onrender.com/api/v1'
+    
     : '/api/v1';
 
 const api = axios.create({
